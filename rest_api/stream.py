bfd0bbe2f350c71e829d5d1e7c5f4cc275968504
james-j-obrien <james.obrien@tpg.com.au>
2019-04-27 21:20:56 +1000

    Remove unnecessary code from stream

--- rest_api/stream.py
+++ rest_api/stream.py
@@ -14,7 +14,4 @@ def vote_stream(db, playlist):
 @bp.route('/', methods=['GET'])
 @playlist_exists
 def r_stream(playlist):
-    db = get_db()
-    if not db.sismember('playlists', playlist):
-        return 'playlist does not exist', 404
-    return Response(vote_stream(db, playlist), mimetype="text/event-stream")
+    return Response(vote_stream(get_db(), playlist), mimetype="text/event-stream")
